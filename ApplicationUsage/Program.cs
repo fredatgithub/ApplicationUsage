2fe052c2c8224d8b5157571411b376744e161a83
fred <domaine75@gmail.com>
2026-02-13 09:52:19 +0100

    add data

--- ApplicationUsage/Program.cs
+++ ApplicationUsage/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static MockDataLibrary.MockData;
 
 namespace ApplicationUsage
 {
@@ -8,6 +9,16 @@ namespace ApplicationUsage
     {
       Action<string> Display = Console.WriteLine;
       Display("Mock data");
+      Display("---------");
+      Display("This application demonstrates the use of the MockDataLibrary to generate mock data for testing purposes.");
+      Display("");
+      Display("The MockDataLibrary provides a simple API to create mock data for various types, including names, addresses, phone numbers, and more. It can be used to generate realistic data for testing applications without the need for real user data.");
+      Display("");
+      Display($"one male first name: {GetRandomFirstName(true)}");
+      Display($"one female first name: {GetRandomFirstName(false)}");
+      Display($"one person first name male or female: {GetRandomFirstName()}");
+
+
 
 
       Display("Press any key to exit...");
