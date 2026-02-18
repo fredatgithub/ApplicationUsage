06c907f69c2c2495c7ee009373f2798ce340d383
fred <domaine75@gmail.com>
2026-02-18 14:54:45 +0100

    add unit tests

---
+++ MockDataLibrary.Tests/TestsMockData.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace MockDataLibrary.Tests
+{
+  public class TestsMockData
+  {
+    [Fact]
+    public void Test_simple()
+    {
+      var resultat = 2 + 3;
+      resultat.Should().Be(5);
+    }
+  }
+}
